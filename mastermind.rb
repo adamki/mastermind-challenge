81028f602af112ccec6a386289f5666c85c469a7
Adam Ki Jensen <adajensen@gmail.com>
2015-11-07 13:17:34 -0700

    WIP

--- mastermind.rb
+++ mastermind.rb
@@ -1,80 +1,72 @@
-
 class Mastermind
-
-  attr_accessor :guesses
+  attr_accessor :guess, :answer
 
   def initialize
-    @guesses = 0
-    color= %w(r g b y)
-    $answer = color.shuffle!
+    @answer = generate_answer
+    get_intro
+    eval_intro
   end
 
-  def self.get_intro
-    puts "Welcome to MASTERMIND\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    @player_response = gets.chomp.downcase
-    eval_intro_response
+  def generate_answer
+    colors = %w(r b g y)
+    color_collection = [colors.sample, colors.sample, colors.sample, colors.sample].join
   end
 
-  def self.eval_intro_response
-    if @player_response == "p" || @player_response == "play"
-      start_game
-    elsif @player_response == "i" || @player_response == "instructions"
-      print_instructions
-      start_game
-    elsif @player_response == "q" || @player_response == "quit"
-      exit_game
-    else
-      puts "try again"
+  def get_intro
+    puts "Welcome to Mastermind.\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def eval_intro
+    input = gets.strip
+      case input
+        when 'play', 'p' then play
+        when 'instructions', 'i' then instructions
+        when 'quit', 'q' then quit
+      else
+        initialize
+      end
     end
   end
 
-  def self.print_instructions
-    puts "....here are instructions....here are instructions....here are instructions"
-  end
-
-  def self.start_game
+  def play
     puts "I have generated a beginner sequence with four elements made up of: (r)ed,
     (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
     What's your guess?"
-    guess = gets.chomp.chars
-    eval_answer(guess)
+    take_turn
   end
 
-  def self.exit_game
+  def instructions
+    puts "I will generate a sequence with four elements made up of different colors. Your job is
+    to guess the correct sequence in as few turns as possible. I will give you hints each turn
+    to help you narrow it down. To play, press p or type play. To quit, press q or type quit."
+    eval_intro
+  end
+
+  def quit
+    puts "Thanks for playing."
     exit!
   end
 
-  def self.eval_answer(guess)
-    guess.sort!
-    $answer.sort!
-
-    if guess == $answer
-      puts "You Win an are amazing"
-      exit_game
-    end
-
-    correct = 0
-    incorrect = 0
-    
-    4.times do |i|
-      if $answer.include?(i)
-        correct += 1
-        guess.shift
-      else
-        incorrect += 1
-        guess.shift
+  def take_turn
+    @guess_counter = 0
+    until @guess == ('q' || 'quit') || (correct_guess)
+      @guess = gets.strip.downcase
+      if @guess == ('q' || 'quit')
+        quit
+      elsif @guess == ('c' || 'cheat')
+        print_secret_code
+        puts "Cheater! But go ahead if you must."
+      elsif @guess.length < 4
+        puts "Guess must be 4 characters. Guess again."
+      elsif @guess.length > 4
+        puts "Guess must be 4 characters. Guess again."
+      elsif correct_guess
+        end_game_flow
+      elsif (@guess.length == 4) && (!correct_guess)
+        puts "#{@guess} has #{correct_elements} matching elements with
+         #{correct_placement} in the correct positions. You've taken #{@guess_counter} guesses.
+          Guess again."
       end
+      @guess_counter += 1
     end
-    summary
   end
-
-  def self.summary
-    puts "Congratulations! You guessed the sequence #{$answer}and it took #{@guesses} geusses."
-  end
-end
-
-
-if __FILE__==$0
-  Mastermind.new
-  Mastermind.get_intro
-end
